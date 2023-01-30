c082dc75824bf25db09ba26d91b8e3b20bd07e87
Atul Kumar <Atul.Kumar@emaratech.ae>
2023-01-30 12:18:37 +0400

    added iam user code in main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,11 @@ resource "aws_s3_bucket_versioning" "versioning_example"{
     }
 }
 
+resource "aws_iam_user" "my_iam_user"{
+
+    name = "terraform_test_user"
+}
+
 output "my_s3_bucket_versioning"{
 
     value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
