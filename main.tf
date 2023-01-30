391251a93ecdaa353002b47b5f23855291ba47a2
Atul Kumar <Atul.Kumar@emaratech.ae>
2023-01-30 15:06:51 +0400

    removed output code from main.tf

--- main.tf
+++ main.tf
@@ -21,19 +21,6 @@ resource "aws_iam_user" "my_iam_user"{
     name = "terraform_test_user_changed"
 }
 
-output "my_s3_bucket_versioning"{
-
-    value = aws_s3_bucket.my_s3_bucket.versioning[0].enabled
-
-
-}
-
-output "my_iam_user_complete_details"{
-
-    value = aws_iam_user.my_iam_user
-
-
-}
 
 
 
