bf0ff7b127773daffbdd6b5c4f054ddd9eea3f91
Atul Kumar <Atul.Kumar@emaratech.ae>
2023-01-30 15:12:39 +0400

    updated main.tf

--- main.tf
+++ main.tf
@@ -2,26 +2,3 @@ provider "aws"{
     region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "my_s3_bucket"{
-
-    bucket = "my-s3-bucket-atul-12345"
-
-}
-
-resource "aws_s3_bucket_versioning" "versioning_example"{
-
-    bucket = aws_s3_bucket.my_s3_bucket.id
-    versioning_configuration{
-        status = "Enabled"
-    }
-}
-
-resource "aws_iam_user" "my_iam_user"{
-
-    name = "terraform_test_user_changed"
-}
-
-
-
-
-
